e1db301058471af2ffb055d600cb9ec8f5f59e72
vuminhhai4000@gmail.com <hvu@sellmark.net>
2021-04-21 11:28:23 -0500

    set from-date sync product

--- app/Console/Commands/SyncProducts.php
+++ app/Console/Commands/SyncProducts.php
@@ -75,9 +75,8 @@ class SyncProducts extends Command
 
     private function setFromDate()
     {
-        if($this->option('from-date') !== null) {
-            $this->inventorySearch->setFromDate($this->option('from-date'));
-        }
+        $fromDate = ($this->option('from-date') === null) ? Carbon::now()->subDay(7) : $this->option('from-date');
+        $this->inventorySearch->setFromDate($fromDate);
     }
 
 }
